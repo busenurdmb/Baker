5957188f2a0095d57a5953198da8aecb9f19d9e4
busenurdmb <bus.dmb@gmail.com>
2026-01-23 21:02:27 +0300

    Add project files.

---
+++ Baker.WebApi/Context/BakerContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Baker.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Baker.WebApi.Context
+{
+    public class BakerContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-493DFJA\\SQLEXPRESS; initial catalog=DbBakerAkademiq ;integrated Security=true; Trustservercertificate=true");  ;
+        }
+
+        public DbSet<Chef> Chefs { get; set; }
+
+        public DbSet<Feature> Features { get; set; }
+
+
+    }
+}
---
+++ Baker.WebUI/Dtos/Chefs/ResultChefDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Baker.WebUI.Dtos.Chefs
+{
+    public class ResultChefDto
+    {
+
+      public int chefId { get; set; }
+      public string name { get; set; }
+      public string title { get; set; }
+      public string imageurl { get; set; }
+       
+
+    }
+}
